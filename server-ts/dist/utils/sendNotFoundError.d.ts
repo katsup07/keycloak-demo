7bc185da6d2e7769d773715abf240eb10157a69b
brett <brett_7y7@yahoo.ca>
2025-08-01 18:21:10 +0900

    distをgitignoreに追加する

--- server-ts/dist/utils/sendNotFoundError.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from 'express';
-export declare const sendNotFoundError: (req: Request, res: Response) => Response<any, Record<string, any>>;
-//# sourceMappingURL=sendNotFoundError.d.ts.map
